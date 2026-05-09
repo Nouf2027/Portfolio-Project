347d4b6775669dbc8ec520b7e62d9ef0d406e037
q400400200-pixe <q.400400200@gmail.com>
2026-05-09 18:30:27 +0200

    Add Navbar, Login, Register pages

--- Stage 4/frontend/src/App.js
+++ Stage 4/frontend/src/App.js
@@ -1,8 +1,21 @@
-import './App.css';
-import CenterCard from "./components/CenterCard";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import CenterCard from './components/CenterCard';
 
 function App() {
-  return <CenterCard />;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<CenterCard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
