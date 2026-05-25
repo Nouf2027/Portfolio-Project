83db84622c2a5194fcb31dcf732c17fbd89b9ce1
Nouf Almutairi <noufn8988@gmail.com>
2026-05-26 01:28:33 +0300

    connect frontend to render backend

--- Stage 4/frontend/src/api/axios.js
+++ Stage 4/frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
-});
+baseURL: 'https://portfolio-project-u0zq.onrender.com/api',});
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
