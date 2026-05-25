a5618b4c0a24fe1d2615c053fffb61d1f4cc7b70
Nouf Almutairi <noufn8988@gmail.com>
2026-05-26 02:05:33 +0300

    fix database connection

--- Stage 4/backend/config/db.js
+++ Stage 4/backend/config/db.js
@@ -2,11 +2,10 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 const createTables = async () => {
@@ -66,8 +65,8 @@ const createTables = async () => {
       );
     `);
     console.log("Tables created successfully");
-  } catch (err) {
-    console.error("Error creating tables:", err.messages);
+   } catch (err) {
+    console.error("Error creating tables:", err.message);
   }
 };
 
