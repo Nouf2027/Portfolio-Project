8fc349ec3e952a745b0fae733d7c206fcc8c80ab
Nouf Almutairi <noufn8988@gmail.com>
2026-06-06 04:21:09 +0300

    fix duplicate path

--- Stage 4/backend/models/Center.js
+++ Stage 4/backend/models/Center.js
@@ -2,7 +2,6 @@ const pool = require('../config/db');
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-const path = require("path");
 
 const uploadDir = path.join(__dirname, "../uploads");
 
