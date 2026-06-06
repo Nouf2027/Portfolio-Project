bcafb30b0ea381fdeb4e040850faf4ba3c7ffef4
Nouf Almutairi <noufn8988@gmail.com>
2026-06-06 04:14:11 +0300

    fix uploads folder

--- Stage 4/backend/models/Center.js
+++ Stage 4/backend/models/Center.js
@@ -1,9 +1,17 @@
 const pool = require('../config/db');
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+const path = require("path");
+
+const uploadDir = path.join(__dirname, "../uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-  destination: "uploads/",
+  destination: uploadDir,
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
   },
