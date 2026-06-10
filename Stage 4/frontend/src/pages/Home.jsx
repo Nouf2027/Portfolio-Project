c25047a338a8bb9e53880b13d6bfe21f65b257c6
Nouf Almutairi <noufn8988@gmail.com>
2026-06-10 11:03:36 +0300

    a

--- Stage 4/frontend/src/pages/Home.jsx
+++ Stage 4/frontend/src/pages/Home.jsx
@@ -104,7 +104,7 @@ function Home() {
                   <p className="center-location">📍 {center.location || "Location not added"}</p>
                   <p className="center-description">{center.description || "No description available."}</p>
                   <div className="center-rating">
-                    ⭐ {center.rating || "4.8"}
+                    ⭐ {center.rating }
                     <span> ({center.reviews_count || 0} reviews)</span>
                   </div>
                   <Link to={`/centers/${center.id || center._id}`} className="details-btn">
