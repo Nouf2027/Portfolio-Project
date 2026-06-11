8f355b727cc8b2300a663170722cd41e99fc5e14
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-06-11 14:47:22 +0300

    Remove Search component and related route
    Removed unused Search component and its route.

--- Stage 4/frontend/src/App.js
+++ Stage 4/frontend/src/App.js
@@ -5,7 +5,6 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Search from "./pages/Search";
 import CenterDetails from "./pages/CenterDetails";
 import Booking from "./pages/Booking";
 import Privacy from "./pages/Privacy";
@@ -26,7 +25,6 @@ return (
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/search" element={<Search />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/centers/:id" element={<CenterDetails />} />
